9bbc0a3bf97a9a0514f26d993ef3e199b4205fc7
ArinaPuzina <161749552+ArinaPuzina@users.noreply.github.com>
2024-11-30 03:31:22 +0700

    1 task

---
+++ barrier.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <thread>
+#include <barrier>
+#include <vector>
+#include <chrono>
+#include <mutex>
+
+#include "support.hpp"
+
+#define N 5
+#define numIter 100
+
+using namespace std;
+
+// Барьер для синхронизации
+barrier sync_point(N);
+
+// Мьютекс для синхронизации вывода
+mutex cout_mutex;
+
+void run() {
+    auto start = chrono::steady_clock::now();
+
+    // Ожидание на барьере
+    sync_point.arrive_and_wait();
+
+    vector<char> random_symbols;
+    for (int i = 0; i < numIter; i++) {
+        random_symbols.push_back(generateRandom());
+    }
+
+    // Синхронизированный вывод
+    {
+        lock_guard<mutex> lock(cout_mutex);
+        for (auto num : random_symbols) {
+            cout << num << " ";
+        }
+        cout << endl;
+    }
+
+    auto end = chrono::steady_clock::now();
+    chrono::duration<double> elapsed = end - start;
+
+    // Вывод времени выполнения
+    {
+        lock_guard<std::mutex> lock(cout_mutex);
+        cout << "Elapsed time: " << elapsed.count() << " seconds" << endl;
+    }
+}
+
+template <typename T>
+void primitive(T func) {
+    vector<thread> threads;
+    for (int i = 0; i < N; i++) {
+        threads.emplace_back(func);
+    }
+
+    for (auto& t : threads) {
+        t.join();
+    }
+}
+
+int main() {
+    primitive(run);
+    return 0;
+}
---
+++ spinlock.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <chrono>
+#include <atomic>
+
+#include "support.hpp"
+
+#define N 5
+#define numIter 100
+
+using namespace std;
+
+class SpinLock {
+    atomic_flag lock_flag = ATOMIC_FLAG_INIT;
+public:
+    void lock() {
+        while (lock_flag.test_and_set(memory_order_acquire)) {}
+    }
+
+    void unlock() {
+        lock_flag.clear(memory_order_release);
+    }
+};
+
+SpinLock spinlock;
+
+void run() {
+    auto start = chrono::steady_clock::now();
+    spinlock.lock();
+    for(int i = 0; i < numIter; i++) {
+        cout << generateRandom() << " ";
+    }
+    cout << endl;
+    auto end = chrono::steady_clock::now();
+    chrono::duration<double> elapsed = end - start;
+    spinlock.unlock();
+    cout << "Elapsed time: " << elapsed.count() << " seconds" << endl;
+}
+
+template <typename T>
+void primitive(T func){
+    vector<thread> threads;
+    for( int i = 0; i < N; i++) {
+        threads.emplace_back(func);
+    }
+
+    for (int i = 0; i < threads.size(); i++) {
+        threads[i].join();
+    }
+}
+
+int main() {
+    primitive(run);
+    return 0;
+}
